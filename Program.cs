8d145e33fb25f91824667ab019af8bb2112d9f89
Amanda Brown <amanda.brown@gmail.com>
2016-09-20 09:20:52 -0400

    slide problem done

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<bool> boolList = new List<bool>() { true, false, false, true, false };
+            foreach(bool value in boolList)
+                if(value == true)
+                {
+                    Console.WriteLine("Better bring an umbrella");
+                }
+                else
+                {
+                    Console.WriteLine("No rain today, enjoy the sun!");
+                }
+            Console.ReadKey();
+        }
+    }
+}
